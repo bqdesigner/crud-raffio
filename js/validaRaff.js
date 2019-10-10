15ff4abdca27a4bb53bdbbae367118e14a7b78bb
Bruno Queirós <bqdesigner@outlook.com>
2019-10-10 15:35:56 -0300

    Update script validaRaff.js

--- js/validaRaff.js
+++ js/validaRaff.js
@@ -1,3 +1,10 @@
+// Validando o envio do Raff
+const desc = document.getElementById('desc_proj').onblur = function() {
+    // e.preventDefault();
+    if (desc.value == '' || desc.value.length <= 3) 
+        alert("Ideia curta.");
+}
+
 // Inserindo o caminho da imagem dentro uma label
 const btnUpload = document.getElementById('imagem_proj');
 function exibeNome(e) {
@@ -8,11 +15,3 @@ function exibeNome(e) {
 }   
 btnUpload.addEventListener('change', exibeNome);
 
-// Validando o envio do Raff
-// document.getElementById('desc_proj').onblur = function() {
-//     // e.preventDefault();
-//     const desc = document.getElementById('desc_proj');
-//     if (desc.length < 2) {
-//         alert("Ideia curta.");
-//     }
-// }
